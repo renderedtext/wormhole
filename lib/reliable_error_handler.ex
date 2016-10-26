a80f909df6a71acb8f6acac5db92479d50fedbb0
Predrag Rakic <prakic@renderedtext.com>
2016-10-26 14:44:34 +0200

    Tests

--- lib/reliable_error_handler.ex
+++ lib/reliable_error_handler.ex
@@ -5,51 +5,83 @@ defmodule ReliableErrorHandler do
 
   @doc """
   Examples:
-      iex> r = handle(fn-> raise "Error" end)
-      iex> r |> elem(0)
-      :error
+      iex> handle(fn-> :a end)
+      {:ok, :a}
 
-      iex> r = handle(fn-> throw "Error" end)
+      iex> r = handle(fn-> raise "Something happened" end)
       iex> r |> elem(0)
       :error
       iex> r |> elem(1) |> elem(0)
-      {:nocatch, "Error"}
+      %RuntimeError{message: "Something happened"}
+
+      iex> r = handle(fn-> throw "Something happened" end)
+      iex> r |> elem(0)
+      :error
+      iex> r |> elem(1) |> elem(0)
+      {:nocatch, "Something happened"}
+
+      iex> handle(fn-> exit :foo end)
+      {:error, :foo}
+
+      iex> handle(fn-> Process.exit(self, :foo) end)
+      {:error, :foo}
   """
   def handle(callback), do:
     handle(callback, @timeout_ms)
 
+  @doc """
+  Examples:
+      iex> handle(Enum, :count, [[]])
+      {:ok, 0}
+  """
+  def handle(module, function, args), do:
+    handle(module, function, args, @timeout_ms)
+
+    @doc """
+    Examples:
+        iex> handle(:timer, :sleep, [20], 50)
+        {:ok, :ok}
+
+        iex> handle(:timer, :sleep, [:infinity], 50)
+        {:error, {:timeout, 50}}
+    """
+  def handle(module, function, args, timeout), do:
+    handle(fn-> apply(module, function, args) end, timeout)
+
+    @doc """
+    Examples:
+        iex> handle(fn-> :timer.sleep 20 end, 50)
+        {:ok, :ok}
+
+        iex> handle(fn-> :timer.sleep :infinity end, 50)
+        {:error, {:timeout, 50}}
+    """
   def handle(callback, timeout) do
-    {pid, monitor} = spawn_monitor(send_ret_val(callback))
+    {pid, monitor} = spawn_monitor(send_return_value(callback))
     receive do
       {:DOWN, ^monitor, :process, ^pid, :normal} ->
-        IO.puts "ok";
-        receive do
-          response ->
-            IO.puts "Response: #{inspect response}"
-            {:ok, response}
-        after timeout -> IO.puts "Weird..."
-        end
+        response_receive(timeout)
       {:DOWN, ^monitor, :process, ^pid, reason}  ->
         Logger.error "Error in handeled function: #{inspect reason}";
         {:error, reason}
     after timeout ->
       Logger.error "Timeout..."
-      {:error, :timeout}
+      {:error, {:timeout, timeout}}
     end
-
-    # IO.puts "Continuing with my life..."
   end
 
-  def handle(module, function, args), do:
-    handle(module, function, args, @timeout_ms)
-
-  def handle(module, function, args, timeout) do
-    handle(fn-> apply(module, function, args) end, timeout)
-  end
-
-  defp send_ret_val(callback) do
+  defp send_return_value(callback) do
     caller_pid = self
-    fn-> send(caller_pid, callback.()) end
+    fn-> send(caller_pid, {__MODULE__, :response, callback.()}) end
   end
 
+  defp response_receive(timeout) do
+    receive do
+      {__MODULE__, :response, response} ->
+        {:ok, response}
+    after timeout ->
+      Logger.error "#{__MODULE__}: Unexpected! Should never get here..."
+      {:error, {:timeout, timeout}}
+    end
+  end
 end
--- test/reliable_error_handler_test.exs
+++ test/reliable_error_handler_test.exs
@@ -2,4 +2,21 @@ defmodule ReliableErrorHandlerTest do
   use ExUnit.Case
   doctest ReliableErrorHandler, [import: true]
 
+  alias ReliableErrorHandler, as: Reh
+
+  test "successful execution - foo, 1 arg" do
+    assert Reh.handle(&foo_function/0) == {:ok, :foo}
+  end
+
+  test "successful execution - foo, 3 arg" do
+    assert Reh.handle(__MODULE__, :foo_function, []) == {:ok, :foo}
+  end
+
+  test "successful execution - bar, 3 arg" do
+    assert Reh.handle(__MODULE__, :bar_function, [4]) == {:ok, {:bar, 4}}
+  end
+
+  def foo_function do :foo end
+
+  def bar_function(arg) do {:bar, arg} end
 end
