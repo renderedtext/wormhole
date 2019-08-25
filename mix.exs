03d35f2dd94ecce0bbb15d4a4b7cc6e3e0660a30
Ethan Larkham <ethanlarkham@gmail.com>
2019-08-25 18:41:33 -0400

    Increment required Elixir verison to 1.7+

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wormhole.Mixfile do
   def project do
     [app: :wormhole,
      version: "2.3.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
